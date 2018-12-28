d665b3d4d4293ee5b2801c6ce1be06c1c8eaf927
Michael Harper <mharper@standalonecode.com>
2018-12-27 18:39:56 -0800

    Initial watchOS app is working
    Scans at app launch and connects.
    Supports disconnect/reconnect.
    Supports toggling relay.
    
    :sunglasses:

--- kevin-ios/Kevin WatchKit Extension/InterfaceController.swift
+++ kevin-ios/Kevin WatchKit Extension/InterfaceController.swift
@@ -12,11 +12,18 @@ import CoreBluetooth
 
 class InterfaceController: WKInterfaceController {
 
-    override func awake(withContext context: Any?) {
+  @IBOutlet weak var toggleRelayButton: WKInterfaceButton!
+  @IBOutlet weak var connectDisconnectButton: WKInterfaceButton!
+  
+  var relayOn = false
+  
+  let kevin = Kevin.shared
+
+  override func awake(withContext context: Any?) {
         super.awake(withContext: context)
-        
-        // Configure interface objects here.
-    }
+    registerForNotifications()
+    configureConnectButton()
+  }
     
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
@@ -28,4 +35,74 @@ class InterfaceController: WKInterfaceController {
         super.didDeactivate()
     }
 
+  @IBAction func connectDisconnectAction() {
+    if let kevinPeripheral = kevin.kevinPeripheral {
+      switch kevinPeripheral.state {
+      case .connected, .connecting:
+        kevin.disconnect()
+      case .disconnected, .disconnecting:
+        kevin.reconnect()
+      default: ()
+      }
+    }
+  }
+  
+  @IBAction func toggleRelayAction() {
+    relayOn = !relayOn
+    kevin.writeRelayCharacteristic(relayOn)
+  }
+  
+  func registerForNotifications() {
+    let notificationCenter = NotificationCenter.default
+    notificationCenter.addObserver(forName: Notification.Name.didConnectToKevin, object: nil, queue: nil) { (notification) in
+      self.configureConnectButton()
+      self.configureToggleButton()
+    }
+    notificationCenter.addObserver(forName: Notification.Name.didDisconnectFromKevin, object: nil, queue: nil) { (notification) in
+      self.configureConnectButton()
+      self.configureToggleButton()
+    }
+    notificationCenter.addObserver(forName: Notification.Name.relayValueChanged, object: nil, queue: nil) { (notification) in
+      self.configureToggleButton()
+    }
+  }
+  
+  func configureConnectButton() {
+    DispatchQueue.main.async {
+      switch self.kevin.kevinPeripheral?.state {
+      case .connected?, .connecting?:
+        self.connectDisconnectButton.setTitle("Disconnect")
+//        self.connectDisconnectButton.setTitleColor(UIColor.white, for: .normal)
+//        self.connectDisconnectButton.backgroundColor = UIColor.blue
+        
+      case .disconnected?, .disconnecting?:
+        self.connectDisconnectButton.setTitle("Connect")
+//        self.connectDisconnectButton.setTitleColor(UIColor.blue, for: .normal)
+//        self.connectDisconnectButton.backgroundColor = UIColor.white
+        
+      default: ()
+      }
+    }
+  }
+  
+  func configureToggleButton() {
+    DispatchQueue.main.async {
+      switch self.kevin.kevinPeripheral?.state {
+      case .connected?:
+        self.toggleRelayButton.setEnabled(true)
+        if let relayValue = self.kevin.relayValue, relayValue {
+          self.toggleRelayButton.setTitle("Turn OFF")
+        }
+        else {
+          self.toggleRelayButton.setTitle("Turn ON")
+        }
+        
+      case .connecting?, .disconnected?, .disconnecting?:
+        self.toggleRelayButton.setEnabled(false)
+        self.toggleRelayButton.setTitle("N/A")
+        
+      default: ()
+      }
+    }
+  }
 }
