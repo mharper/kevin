e932ec0fe211d813f7c5f1534c9d7838fe616fbd
Michael Harper <mharper@standalonecode.com>
2018-12-29 10:32:30 -0800

    tvOS support
    Adds minimal functionality that actually works on an Apple TV.

---
+++ kevin-ios/KevinCam/AppDelegate.swift
@@ -0,0 +1,45 @@
+//
+//  AppDelegate.swift
+//  KevinCam
+//
+//  Created by Michael Harper on 12/28/18.
+//  Copyright © 2018 Michael Harper. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    Kevin.shared.initBluetooth()
+  return true
+  }
+
+  func applicationWillResignActive(_ application: UIApplication) {
+    Kevin.shared.disconnect()
+}
+
+  func applicationDidEnterBackground(_ application: UIApplication) {
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+  }
+
+  func applicationWillEnterForeground(_ application: UIApplication) {
+    Kevin.shared.reconnect()
+ }
+
+  func applicationDidBecomeActive(_ application: UIApplication) {
+    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+  }
+
+  func applicationWillTerminate(_ application: UIApplication) {
+    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+  }
+
+
+}
+
